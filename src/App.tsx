b9b7946e07ecdc44c1ed8023b24ccd4c0c97fa75
Joacim Andersson <joacim@josiasmusik.se>
2024-02-23 10:11:02 +0100

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Card } from "./components/Card";
 import { Grid } from "./components/Grid";
 
 function App() {
   return (
     <>
       <Grid />
+      <Card />
     </>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+import '../styles/card.css';
+
+export const Card = () => {
+  return (
+    <div className="container">
+      <div className="card-container">
+        <div className="card">
+          <div className="front-card"></div>
+          <div className="back-card"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
